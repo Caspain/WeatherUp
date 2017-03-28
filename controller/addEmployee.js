13d3439e78f4891020d0be86babdefbc81d4af12
remario <remariorich@gmail.com>
2017-03-27 21:44:15 -0500

    installed axios

--- controller/addEmployee.js
+++ controller/addEmployee.js
@@ -1,16 +1,21 @@
 const Promise = require('bluebird');
 const moment = require('moment');
-
+const randomstring = require("randomstring");
 module.exports = function(data) {
     return new Promise((resolve, reject) => {
-      let time = null;
-      let data = null;
-      let identifier = null;
-      let time = moment().format("h:mm:ss a");
-      let date = moment().format("dddd, MMMM Do YYYY");
+        let time = null;
+        let data = null;
+        let identifier = null;
+        let time = moment().format("h:mm:ss a");
+        let date = moment().format("dddd, MMMM Do YYYY");
 
-      data.time_created = time;
-      data.date_created = date;
+        data.time_created = time;
+        data.date_created = date;
+        data.identifier = randomstring.generate({
+            length: 12,
+            charset: 'alphanumeric'
+        });
+
         const promise = require('../database/addEmployee')(data);
         promise.then((result) => {
             resolve(result);
