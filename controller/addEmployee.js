b9c4ab26d7b3dacad894b19a9f430545c4e6ffb6
remario <remariorich@gmail.com>
2017-03-27 21:41:32 -0500

    added randomString and moment.js

--- controller/addEmployee.js
+++ controller/addEmployee.js
@@ -1,6 +1,16 @@
 const Promise = require('bluebird');
+const moment = require('moment');
+
 module.exports = function(data) {
     return new Promise((resolve, reject) => {
+      let time = null;
+      let data = null;
+      let identifier = null;
+      let time = moment().format("h:mm:ss a");
+      let date = moment().format("dddd, MMMM Do YYYY");
+
+      data.time_created = time;
+      data.date_created = date;
         const promise = require('../database/addEmployee')(data);
         promise.then((result) => {
             resolve(result);
--- router/router.js
+++ router/router.js
@@ -1,5 +1,6 @@
 const constants = require('../constants/constants');
 module.exports = function(express) {
+
     const router = express.Router();
     router.route('/').get(function(req, res) {
         res.render('main')
