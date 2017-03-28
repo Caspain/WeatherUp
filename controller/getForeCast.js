86ac887876c4f1e6da95f0664d26d8aa937b24ea
remario <remariorich@gmail.com>
2017-03-28 12:09:31 -0500

    added route for getting forecast by city

--- controller/getForeCast.js
+++ controller/getForeCast.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const config = require('../configuration/config');
 module.exports = function(data) {
     return new Promise((resolve, reject) => {
-
+console.log(config.forecast_kingston_api)
       axios.get(config.forecast_kingston_api)
         .then(function (response) {
            resolve(response);
