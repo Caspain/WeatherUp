31e1dbd9512d796a378db6c8b47fb6ec18544e78
remario <remariorich@gmail.com>
2017-03-28 12:24:06 -0500

    switched to request

--- controller/getForeCast.js
+++ controller/getForeCast.js
@@ -4,15 +4,13 @@ const config = require('../configuration/config');
 module.exports = function(data) {
     return new Promise((resolve, reject) => {
 
-      request('http://www.google.com', function (error, response, body) {
-        console.log('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body); // Print the HTML for the Google homepage.
-      });
-        // promise.then((result) => {
-        //     resolve(result);
-        // }).catch((error) => {
-        //     reject(error);
-        // })
+        request(config.forecast_kingston_api, function(error, response, body) {
+            if (error) {
+                reject(error);
+            } else {
+                resolve(body)
+            }
+        });
+
     });
 };
